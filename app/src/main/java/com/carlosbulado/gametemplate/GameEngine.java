14ddb87048d8a2c847fad5c2f0fddcc16a896bf2
Carlos José Bulado <carlosbulado@gmail.com>
2019-09-25 12:11:44 -0400

    Initial commit for Tappy Spaceship

--- app/src/main/java/com/carlosbulado/gametemplate/GameEngine.java
+++ app/src/main/java/com/carlosbulado/gametemplate/GameEngine.java
@@ -1,6 +1,8 @@
 package com.carlosbulado.gametemplate;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -9,10 +11,12 @@ import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
+import java.util.Random;
+
 public class GameEngine extends SurfaceView implements Runnable
 {
     // Android debug variables
-    static String TAG = "GAME TEMPLATE";
+    static String TAG = "TAPPY SPACESHIP";
 
     // screen size
     int screenHeight;
@@ -37,6 +41,12 @@ public class GameEngine extends SurfaceView implements Runnable
     // ## SPRITES
     // ------------------------------------------------
 
+    int playerXPosition;
+    int playerYPosition;
+
+    int enemyXPosition;
+    int enemyYPosition;
+
     // ------------------------------------------------
     // ## GAME STATS
     // ------------------------------------------------
@@ -55,11 +65,29 @@ public class GameEngine extends SurfaceView implements Runnable
 
         // @TODO: Add your sprites
 
+        // put initial starting postion of enemy
+        this.enemyXPosition = 1300;
+        this.enemyYPosition = 120;
+
+        // put the initial starting position of your player
+        this.playerXPosition = 100;
+        this.playerYPosition = 600;
+
         // @TODO: Any other game setup
 
     }
 
     private void printScreenInfo() { Log.d(TAG, "Screen (w, h) = " + this.screenWidth + "," + this.screenHeight); }
+
+    private void spawnPlayer() {
+        //@TODO: Start the player at the left side of screen
+    }
+    private void spawnEnemyShips() {
+        Random random = new Random();
+
+        //@TODO: Place the enemies in a random location
+
+    }
 
     // ------------------------------------------------
     // GAME STATE FUNCTIONS (run, stop, start)
@@ -97,13 +125,29 @@ public class GameEngine extends SurfaceView implements Runnable
     public void updatePositions()
     {
         // @TODO: Update position of all sprites
+        if (this.fingerAction == "mousedown") {
+            // if mousedown, then move player up
+            // Make the UP movement > than down movement - this will
+            // make it look like the player is moving up alot
+            this.playerYPosition = this.playerYPosition - 100;
+        }
 
+        if (this.fingerAction == "mouseup") {
+            // if mouseup, then move player down
+            this.playerYPosition = this.playerYPosition + 10;
+        }
         // @TODO: Logic of the game
+        this.enemyXPosition = this.enemyXPosition - 25;
+
+        if (this.enemyXPosition <= 0) {
+            // restart the enemy in the starting position
+            this.enemyXPosition = 1300;
+            this.enemyYPosition = 120;
+        }
 
         //@TODO: Test lose conditions
 
         //@TODO: Test win conditions
-
     }
 
     public void redrawSprites()
@@ -118,14 +162,20 @@ public class GameEngine extends SurfaceView implements Runnable
             this.canvas.drawColor(Color.WHITE);
             this.paintbrush.setColor(Color.BLACK);
 
+            paintbrush.setColor(Color.BLUE);
+            paintbrush.setStyle(Paint.Style.STROKE);
+            paintbrush.setStrokeWidth(5);
+
             //@TODO: Draw sprites
-            this.paintbrush.setColor(Color.GREEN);
-            this.canvas.drawRect(100, 600, 400, 900, this.paintbrush);
+            Bitmap playerImage = BitmapFactory.decodeResource(this.getContext().getResources(),
+                    R.drawable.player_ship);
+            canvas.drawBitmap(playerImage, playerXPosition, playerYPosition, paintbrush);
+
+            Bitmap ememyImage = BitmapFactory.decodeResource(this.getContext().getResources(),
+                    R.drawable.alien_ship2);
+            canvas.drawBitmap(ememyImage, enemyXPosition, enemyYPosition, paintbrush);
 
             //@TODO: Draw game stats
-            this.paintbrush.setTextSize(80);
-            this.paintbrush.setColor(Color.BLUE);
-            this.canvas.drawText("This is just a game template", 50, 500, this.paintbrush);
 
             // ------------------------------------------------
             this.holder.unlockCanvasAndPost(canvas);
@@ -144,6 +194,8 @@ public class GameEngine extends SurfaceView implements Runnable
     // USER INPUT FUNCTIONS
     // ------------------------------------------------
 
+    String fingerAction = "";
+
     @Override
     public boolean onTouchEvent(MotionEvent event)
     {
@@ -151,11 +203,11 @@ public class GameEngine extends SurfaceView implements Runnable
         //@TODO: What should happen when person touches the screen?
         if (userAction == MotionEvent.ACTION_DOWN)
         {
-
+            fingerAction = "mousedown";
         }
         else if (userAction == MotionEvent.ACTION_UP)
         {
-
+            fingerAction = "mouseup";
         }
 
         return true;
